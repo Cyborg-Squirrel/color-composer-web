4297e77ddad93136470c03c219fa7c7c136b57d3
Cyborg-Squirrel <ashlybelka@gmail.com>
2025-08-02 15:30:01 -0500

    Added vitest

---
+++ test/main.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+
+// Placeholder test so npm test doesn't throw an error
+describe('group', () => {
+    it('should', () => {
+        expect(1).toBeTruthy();
+    });
+});
