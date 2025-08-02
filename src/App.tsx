77030712ddd13afeffb3a96b4a600ee185af1cc0
Cyborg-Squirrel <ashlybelka@gmail.com>
2025-08-02 09:07:05 -0500

    Recreated project using vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
 import '@mantine/core/styles.css';
+import { useState } from 'react';
 import './App.css';
-import logo from './logo.svg';
-
-import { createTheme, MantineProvider } from '@mantine/core';
-
-const theme = createTheme({
-  /** TODO Put your mantine theme override here */
-});
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <MantineProvider theme={theme}>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-      </header>
-    </div>
-    </MantineProvider>
-  );
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
