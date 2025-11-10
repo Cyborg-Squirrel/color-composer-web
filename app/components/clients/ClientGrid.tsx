4407f8b4f988283daf7a77bdeedbc8412e054b36
Cyborg-Squirrel <ashlybelka@gmail.com>
2025-11-10 15:22:06 -0600

    Clients page ui update, split last seen timestamp out into separate file

--- app/components/clients/ClientGrid.tsx
+++ app/components/clients/ClientGrid.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { ClientStatus, getClients, type ILedStripClient } from "~/api/clients_api";
 import { getStrips, type ILedStrip } from "~/api/strips_api";
 import { BoundedLoadingOverlay } from "../BoundedLoadingOverlay";
+import { getLastSeenAtString } from "../LastSeenAt";
 import { statusColors } from "../status";
 import classes from './ClientGrid.module.css';
 
@@ -95,51 +96,6 @@ function SpanRow(props: { startingText: string, endingText: string }) {
         <Text fw={700} span>{props.startingText}</Text>
         <Text className={classes.subtle_text} span>{props.endingText}</Text>
     </span>
-}
-
-function getLastSeenAtString(lastSeenAt: number) {
-    let now = new Date();
-    let diff = now.getTime() - lastSeenAt;
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
-
-    let yesterday = new Date(now.getTime() - day);
-    yesterday.setHours(0);
-    yesterday.setMinutes(0);
-    yesterday.setSeconds(0);
-    yesterday.setMilliseconds(0);
-
-    let twoDaysAgo = new Date(now.getTime() - (day * 2));
-    twoDaysAgo.setHours(0);
-    twoDaysAgo.setMinutes(0);
-    twoDaysAgo.setSeconds(0);
-    twoDaysAgo.setMilliseconds(0);
-
-    if (diff < second) {
-        return 'now';
-    } else if (diff < minute) {
-        let seconds = Math.trunc(diff / second)
-        if (seconds == 1) {
-            return '1 second ago'
-        } else {
-            return seconds + ' seconds ago'
-        }
-    } else if (diff < hour) {
-        let minutes = Math.trunc(diff / minute)
-        if (minutes == 1) {
-            return '1 minute ago'
-        } else {
-            return minutes + ' minutes ago'
-        }
-    } else if (lastSeenAt <= yesterday.getTime() && lastSeenAt > twoDaysAgo.getTime()) {
-        return 'yesterday';
-    } else if (lastSeenAt <= twoDaysAgo.getTime()) {
-        return new Date(lastSeenAt).toLocaleDateString();
-    }
-
-    return new Date(lastSeenAt).toLocaleTimeString();
 }
 
 function getStatusText(status: ClientStatus) {
--- app/components/clients/ClientTable.tsx
+++ app/components/clients/ClientTable.tsx
@@ -1,17 +1,15 @@
-import { Button, Center, Menu, Table } from "@mantine/core";
+import { Button, Center, Menu, Table, Text } from "@mantine/core";
 import { useContext, useEffect, useState } from "react";
 import { getClients, type ILedStripClient } from "~/api/clients_api";
+import { IsLightModeContext } from "~/context/IsLightModeContext";
 import { IsMobileContext } from "../../context/IsMobileContext";
 import { BoundedLoadingOverlay } from "../BoundedLoadingOverlay";
+import { getLastSeenAtString } from "../LastSeenAt";
 import { statusColors } from "../status";
 
-const MAX_ADDRESS_LENGTH = 15;
-const ELIPSES_LENGTH = 3;
-
-interface IClientTableProps {}
-
-export function ClientTable(props: IClientTableProps) {
+export function ClientTable() {
     const isMobile = useContext(IsMobileContext);
+    const isLightMode = useContext(IsLightModeContext);
     const [clients, setClients] = useState<ILedStripClient[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -27,63 +25,76 @@ export function ClientTable(props: IClientTableProps) {
             });
         }
 
-        return () => {};
+        return () => { };
     }, []);
 
     if (loading) {
-        return <BoundedLoadingOverlay loading/>;
+        return <BoundedLoadingOverlay loading />;
     } else if (error) {
         console.log('Showing error ' + error);
-       return <Center>
+        return <Center>
             <div>
                 Error fetching clients
             </div>
-        </Center>
+        </Center>;
     }
-    
+
     const rows = clients.map((c) => (
-        <Table.Tr key={c.uuid}>
-            <Table.Td>{c.name}</Table.Td>
+        <Table.Tr key={c.uuid} onClick={() => onRowClicked(c.uuid, isMobile)}>
+            {getClientNameTd(c, isMobile, isLightMode)}
+            <Table.Td><Text truncate="end">{c.address}</Text></Table.Td>
             {isMobile ? null : <Table.Td>{c.clientType}</Table.Td>}
-            <Table.Td>{isMobile ? getShortAddress(c) : c.address}</Table.Td>
             <Table.Td c={statusColors.ok}>Idle</Table.Td>
-            <Table.Td>
-                <Menu shadow="md" width="8em" position="bottom-end" closeOnItemClick>
-                    <Menu.Target>
-                        <Button variant="subtle" size="xs" color="var(--mantine-color-text)">...</Button>
-                    </Menu.Target>
-                    <Menu.Dropdown>
-                        <Menu.Item>Edit</Menu.Item>
-                        <Menu.Item>Delete</Menu.Item>
-                    </Menu.Dropdown>
-                </Menu>
-            </Table.Td>
+            {getEditButtonTd(isMobile)}
         </Table.Tr>
     ));
 
     return (
-        <Table verticalSpacing="xs" highlightOnHover={!isMobile}>
+        <Table verticalSpacing="xs" highlightOnHover={true}>
             <Table.Thead>
                 <Table.Tr>
                     <Table.Th>Name</Table.Th>
-                    {isMobile ? null : <Table.Th>Type</Table.Th>}
                     <Table.Th>Address</Table.Th>
+                    {isMobile ? null : <Table.Th>Type</Table.Th>}
                     <Table.Th>Status</Table.Th>
-                    <Table.Th/>
+                    {isMobile ? null : <Table.Th />}
                 </Table.Tr>
             </Table.Thead>
             <Table.Tbody>
-                { rows }
+                {rows}
             </Table.Tbody>
         </Table>
     );
 }
 
-function getShortAddress(client: ILedStripClient): string {
-    let addressWithSchemeRemoved = client.address.replace(/^https?:\/\//, "");
-    if (addressWithSchemeRemoved.length > MAX_ADDRESS_LENGTH) {
-        return addressWithSchemeRemoved.substring(0, MAX_ADDRESS_LENGTH - ELIPSES_LENGTH) + '...';
-    } else {
-        return addressWithSchemeRemoved;
+// TODO: open edit ui if mobile, desktop has the three dot menu
+function onRowClicked(clientUuid: string, isMobile: boolean) {
+    if (isMobile) {
+        console.log("Client clicked:", clientUuid);
     }
+}
+
+function getEditButtonTd(isMobile: boolean) {
+    if (isMobile) {
+        return null;
+    } else {
+        return <Table.Td>
+            <Menu shadow="md" width="8em" position="bottom-end" closeOnItemClick>
+                <Menu.Target>
+                    <Button variant="subtle" size="xs" color="var(--mantine-color-text)">...</Button>
+                </Menu.Target>
+                <Menu.Dropdown>
+                    <Menu.Item>Edit</Menu.Item>
+                    <Menu.Item>Delete</Menu.Item>
+                </Menu.Dropdown>
+            </Menu>
+        </Table.Td>;
+    }
+}
+
+function getClientNameTd(client: ILedStripClient, isMobile: boolean, isLightMode: boolean) {
+    return <Table.Td>
+        <Text>{client.name}</Text>
+        <Text c={isLightMode ? undefined : "dimmed"} size="sm">Last seen: {getLastSeenAtString(client.lastSeenAt)}</Text>
+    </Table.Td>;
 }
